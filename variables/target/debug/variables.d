5b838116845957282def2e193435f0f6f0bc1661
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2020-03-22 01:00:47 -0700

    through ownership, borrowing, references and slices

---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/jordanrowland/projects/rust/variables/target/debug/variables: /Users/jordanrowland/projects/rust/variables/src/main.rs
---
+++ variables/target/rls/debug/deps/variables-abb1a16d9ea02b2b.d
@@ -0,0 +1,5 @@
+/Users/jordanrowland/projects/rust/variables/target/rls/debug/deps/variables-abb1a16d9ea02b2b.rmeta: src/main.rs
+
+/Users/jordanrowland/projects/rust/variables/target/rls/debug/deps/variables-abb1a16d9ea02b2b.d: src/main.rs
+
+src/main.rs:
