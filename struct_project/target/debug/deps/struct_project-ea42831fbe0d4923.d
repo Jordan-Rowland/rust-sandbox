43fca03d09f857a9b56908fb512453ac39063c9b
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2020-03-23 23:42:39 -0700

    removes targets

--- struct_project/target/debug/deps/struct_project-ea42831fbe0d4923.d
+++
@@ -1,5 +0,0 @@
-/Users/jordanrowland/projects/rust/struct_project/target/debug/deps/struct_project-ea42831fbe0d4923: src/main.rs
-
-/Users/jordanrowland/projects/rust/struct_project/target/debug/deps/struct_project-ea42831fbe0d4923.d: src/main.rs
-
-src/main.rs:
--- struct_project/target/debug/struct_project.d
+++
@@ -1,1 +0,0 @@
-/Users/jordanrowland/projects/rust/struct_project/target/debug/struct_project: /Users/jordanrowland/projects/rust/struct_project/src/main.rs
